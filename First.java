597a0249836b55d73180fc6897607c202f5bbdd6
Rutuj Yambal <142421950+RutujJain@users.noreply.github.com>
2024-11-11 16:03:38 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,30 +1,5 @@
-public class PalindromeChecker {
-
-    public static boolean isPalindrome(String str) {
-        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
-    }
-
+public class FirstJavaProgram {
     public static void main(String[] args) {
-        String[] testStrings = {"racecar", "hello", "Madam", "A man, a plan, a canal, Panama", "12321"};
-
-        for (String str : testStrings) {
-            if (isPalindrome(str)) {
-                System.out.println("\"" + str + "\" is a palindrome.");
-            } else {
-                System.out.println("\"" + str + "\" is not a palindrome.");
-            }
-        }
+        System.out.println("This is my first Java program connected with Jenkins.");
     }
 }
